64ef6f709a5860a740af03b7a09cb364f0b05256
Luke Chen Shui <chenshuiluke@gmail.com>
2018-01-13 04:30:54 +0000

    Changed navbar colour

--- app/views/partials/navbar.php
+++ app/views/partials/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-light navbar-expand-lg">
   <a class="navbar-brand" href="/">4 Weeks of Code</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -6,10 +6,6 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-
       <?php
     if(isset($data) && isset($data['github_login_url'])){
 ?>
